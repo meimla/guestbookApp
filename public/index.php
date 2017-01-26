42ec8c17d01764086228acc9f9dbeb0fffd97edc
mihkel <mihkeleimla@gmail.com>
2017-01-26 21:46:29 +0200

    First commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    require_once '/home/vagrant/Code/guestbook/vendor/autoload.php';
+
+    echo "Hello World";
+
+    $database = new medoo([
+        'database_type' => 'sqlite',
+        'database_file' => '../storage/database.db'
+    ]);
+
+    $array = ['1' => 'Hello', '2' => 'World'];
+
+    var_dump($array);
+
+    dump($array);
+
+    echo $array[1] . $array[2];
